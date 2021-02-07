d5613d121b5213d2755153cfc5f4e190a4e7c409
shinyay <shinya.com@gmail.com>
2021-02-07 22:47:45 +0900

    Add Repository annoatation

--- src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/google/shinyay/repository/EmployeeRepository.kt
@@ -2,5 +2,7 @@ package com.google.shinyay.repository
 
 import com.google.shinyay.entity.Employee
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface EmployeeRepository : CrudRepository<Employee, Long>
