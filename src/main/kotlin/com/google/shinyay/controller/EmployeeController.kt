a67ad1ce2d34bda51327849b8ceed77a42ee13f5
shinyay <shinya.com@gmail.com>
2021-02-07 22:51:10 +0900

    Add find all employees

--- src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
@@ -1,5 +1,6 @@
 package com.google.shinyay.controller
 
+import com.google.shinyay.entity.Employee
 import com.google.shinyay.repository.EmployeeRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,4 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1")
 class EmployeeController(val repository: EmployeeRepository) {
 
+    fun findAll(): MutableIterable<Employee> {
+        return repository.findAll()
+    }
 }
