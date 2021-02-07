f373b0f02572ce61e78ed41d1f876e447d452a70
shinyay <shinya.com@gmail.com>
2021-02-07 22:49:26 +0900

    Add Class Level URI

--- src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
@@ -1,8 +1,12 @@
 package com.google.shinyay.controller
 
 import com.google.shinyay.repository.EmployeeRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1")
 class EmployeeController(val repository: EmployeeRepository) {
+
 }
