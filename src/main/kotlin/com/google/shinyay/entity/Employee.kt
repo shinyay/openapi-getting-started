474aed2025291564f5411ef566d4547c5ce4fba4
shinyay <shinya.com@gmail.com>
2021-02-07 22:32:45 +0900

    Add Entity class as Data Class

---
+++ src/main/kotlin/com/google/shinyay/entity/Employee.kt
@@ -0,0 +1,6 @@
+package com.google.shinyay.entity
+
+data class Employee(val id: Long,
+                    val firstName: String,
+                    val lastName: String,
+                    val role: String)
