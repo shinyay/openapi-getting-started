c2c7002897316cb19e162a9c566c0284b26fbf76
shinyay <shinya.com@gmail.com>
2021-02-07 22:48:18 +0900

    Add Constructor Injection for Repository

--- src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
@@ -1,7 +1,8 @@
 package com.google.shinyay.controller
 
+import com.google.shinyay.repository.EmployeeRepository
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class EmployeeController {
+class EmployeeController(val repository: EmployeeRepository) {
 }
