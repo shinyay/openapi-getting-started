3f50526d76f82914bfb230b0195003c898921b10
shinyay <shinya.com@gmail.com>
2021-02-12 10:26:25 +0900

    Add OpenAPI Deifinition

--- src/main/kotlin/com/google/shinyay/OpenapiGettingStartedApplication.kt
+++ src/main/kotlin/com/google/shinyay/OpenapiGettingStartedApplication.kt
@@ -1,10 +1,16 @@
 package com.google.shinyay
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(info = Info(
+		description = "Employee API",
+		version = "1.0"
+))
 @SpringBootApplication
 class OpenapiGettingStartedApplication
 
